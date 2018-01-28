f3bc1c92548dc960c4a3f2122c80b953db116d6c
PetePhx <pete0phx@yandex.com>
2018-01-27 22:59:32 -0700

    minor refactoring of #9

--- medium_1/09_clock.rb
+++ medium_1/09_clock.rb
@@ -10,10 +10,10 @@ the same time should be equal to each other.
 =end
 
 class Clock
-  MINUTES_IN_DAY = 60 * 24
+  MINUTES_IN_A_DAY = 60 * 24
 
   def initialize(hour, minute)
-    @total_minutes = (hour * 60 + minute) % MINUTES_IN_DAY
+    @total_minutes = (hour * 60 + minute) % MINUTES_IN_A_DAY
   end
 
   def self.at(hour = 0, minute = 0)
@@ -21,18 +21,16 @@ class Clock
   end
 
   def to_s
-    hour, minute = @total_minutes.divmod(60)
-    format("%02d:%02d", hour, minute)
+    format("%02d:%02d", *@total_minutes.divmod(60))
   end
 
   def +(minutes)
-    hour, minute = ((@total_minutes + minutes) % MINUTES_IN_DAY).divmod(60)
+    hour, minute = ((@total_minutes + minutes) % MINUTES_IN_A_DAY).divmod(60)
     Clock.new(hour, minute)
   end
 
   def -(minutes)
-    hour, minute = ((@total_minutes - minutes) % MINUTES_IN_DAY).divmod(60)
-    Clock.new(hour, minute)
+    self.+(-minutes)
   end
 
   def ==(other)
