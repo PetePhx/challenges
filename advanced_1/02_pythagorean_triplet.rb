2a6a52746bafea8ac9a82282c16f41b0fe849080
PetePhx <pete0phx@yandex.com>
2018-02-02 14:18:02 -0700

    minor refactor advanced-1 #2

--- advanced_1/02_pythagorean_triplet.rb
+++ advanced_1/02_pythagorean_triplet.rb
@@ -26,11 +26,11 @@ class Triplet
     @nums[0]**2 + @nums[1]**2 == @nums[2]**2
   end
 
-  def self.where(min_factor: 1, max_factor: 10, sum: 0)
-    (min_factor..max_factor).to_a.combination(3).to_a
+  def self.where(min_factor: 1, max_factor: 10, sum: nil)
+    (min_factor..max_factor).to_a.combination(3)
                             .select { |a, b, c| c < a + b }
                             .map { |a, b, c| new(a, b, c) }
-                            .select { |trp| sum > 0 ? trp.sum == sum : true }
+                            .select { |trp| sum.nil? ? true : trp.sum == sum }
                             .select(&:pythagorean?)
   end
 end
