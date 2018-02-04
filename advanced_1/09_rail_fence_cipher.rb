5286aee9bf26d3a6640d890b23dca7e212b4be5b
PetePhx <pete0phx@yandex.com>
2018-02-03 22:14:57 -0700

    refactor advanced-1 #9 rail fence cipher

--- advanced_1/09_rail_fence_cipher.rb
+++ advanced_1/09_rail_fence_cipher.rb
@@ -1,15 +1,18 @@
 =begin
 
-
 Rail Fence Cipher
 
 Implement encoding and decoding for the rail fence cipher.
 
-The Rail Fence cipher is a form of transposition cipher that gets its name from the way in which it's encoded. It was already used by the ancient Greeks.
+The Rail Fence cipher is a form of transposition cipher that gets its name from
+the way in which it's encoded. It was already used by the ancient Greeks.
 
-In the Rail Fence cipher, the message is written downwards on successive "rails" of an imaginary fence, then moving up when we get to the bottom (like a zig-zag). Finally the message is then read off in rows.
+In the Rail Fence cipher, the message is written downwards on successive "rails"
+of an imaginary fence, then moving up when we get to the bottom (like a
+zig-zag). Finally the message is then read off in rows.
 
-For example, using three "rails" and the message "WE ARE DISCOVERED FLEE AT ONCE", the cipherer writes out:
+For example, using three "rails" and the message "WE ARE DISCOVERED FLEE AT
+ONCE", the cipherer writes out:
 
 W . . . E . . . C . . . R . . . L . . . T . . . E
 . E . R . D . S . O . E . E . F . E . A . O . C .
@@ -19,7 +22,8 @@ Then reads off:
 
 WECRLTEERDSOEEFEAOCAIVDEN
 
-To decrypt a message you take the zig-zag shape and fill the ciphertext along the rows.
+To decrypt a message you take the zig-zag shape and fill the ciphertext along
+the rows.
 
 ? . . . ? . . . ? . . . ? . . . ? . . . ? . . . ?
 . ? . ? . ? . ? . ? . ? . ? . ? . ? . ? . ? . ? .
@@ -50,14 +54,14 @@ If you now read along the zig-zag shape you can read the original message.
 class RailFenceCipher
   def self.encode(str, n)
     return str if n == 1
-    idx_arr = build_idx_arr(str.size, n)
+    p idx_arr = build_idx_arr(str.size, n)
     str.chars.values_at(*idx_arr).join
   end
 
   def self.decode(str, n)
     return str if n == 1
     idx_arr = build_idx_arr(str.size, n)
-    (0...str.size).each_with_object([''] * str.size) do |idx, out|
+    (0...str.size).each_with_object([]) do |idx, out|
       out[idx_arr[idx]] = str[idx]
     end.join
   end
@@ -65,15 +69,9 @@ class RailFenceCipher
   def self.build_idx_arr(str_size, n)
     rail_idx = 0.upto(n - 1).to_a + (n - 2).downto(1).to_a
     str_idx = 0...str_size
-    idx_arr = str_idx.map { |i| rail_idx[i % (2 * n - 2)] }
-                     .zip(str_idx)
-                     .sort
-                     .map(&:last)
+    str_idx.map { |i| rail_idx[i % (2 * n - 2)] }
+           .zip(str_idx)
+           .sort
+           .map(&:last)
   end
 end
-
-# p RailFenceCipher.encode('WEAREDISCOVEREDFLEEATONCE', 3)
-# p RailFenceCipher.encode("WECRLTEERDSOEEFEAOCAIVDEN", 3)
-# p RailFenceCipher.encode("ABCDEFGHIJKLMNOP", 3)
-# p RailFenceCipher.decode("AEIMBDFHJLNPCGKO", 3)
-# p RailFenceCipher.decode("ABCDEFGHIJKLMNOP", 3)
